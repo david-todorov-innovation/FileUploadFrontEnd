a5225bd8a103ac049c14f1592bada735720d8fd9
David Todorov <todorov_david@yahoo.com>
2021-12-06 22:13:20 +0100

    adjusted html for the form and file input

--- listener.js
+++ listener.js
@@ -3,9 +3,27 @@ showFile = () => {
 
     const reader = new FileReader();
     reader.onload = event => {
-        let textFromFile = event.target.result;
-        document.getElementById("textArea").textContent = textFromFile;
+        document.getElementById("textArea").textContent = event.target.result;
     }
 
     reader.readAsText(fileUploaded, "UTF-8")
+//
+//     const formData = new FormData();
+//
+//     formData.append("username", "david.todorov");
+//     formData.append("accountnum", 123456); // number 123456 is immediately converted to a string "123456"
+//
+// // HTML file input, chosen by user
+//     formData.append("file", document.getElementById("file-uploader").files[0]);
+//
+// // JavaScript file-like object
+//     let content = '<a id="a"><b id="b">hey!</b></a>'; // the body of the new file...
+//     const blob = new Blob([content], {type: "text/xml"});
+//
+//     formData.append("fileContent", blob);
+//
+//     const request = new XMLHttpRequest();
+//     request.open("POST", "http://localhost:8080/upload");
+//     request.send(formData);
+
 }
